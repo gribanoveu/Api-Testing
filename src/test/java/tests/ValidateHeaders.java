cacf5d0b9e6f7ec085808685324a7181159998cc
Evgeny Gribanov <gribanoveu@yandex.ru>
2021-12-21 12:39:20 +0300

    setup

--- src/test/java/tests/ValidateHeaders.java
+++ src/test/java/tests/ValidateHeaders.java
@@ -1,21 +1,49 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
 
 public class ValidateHeaders {
 
-    @Test
-    public void getDbConnectionValidateHeaders() {}
+    @Test(groups = "HEADERS")
+    @Feature("Проверить заголовки")
+    @Story("TA-1В-1")
+    @Description("Заголовки ответа на GET запрос получения sessionID ссответствуют ожидаемым")
+    public void getDbConnectionValidateHeaders() {
+        // TODO
+    }
 
-    @Test
-    public void getFormByIdValidateHeaders() {}
+    @Test(groups = "HEADERS")
+    @Feature("Проверить заголовки")
+    @Story("TA-1В-2")
+    @Description("Заголовки ответа на GET запрос списка форм ссответствуют ожидаемым")
+    public void getFormsValidateHeaders() {
+        // TODO
+    }
 
-    @Test
-    public void getFormFiltersValidateHeaders() {}
+    @Test(groups = "HEADERS")
+    @Feature("Проверить заголовки")
+    @Story("TA-1В-3")
+    @Description("Заголовки ответа на GET запрос формы по id ссответствуют ожидаемым")
+    public void getFormByIdValidateHeaders() {
+        // TODO
+    }
 
-    @Test
-    public void getFormsValidateHeaders() {}
+    @Test(groups = "HEADERS")
+    @Feature("Проверить заголовки")
+    @Story("TA-1В-4")
+    @Description("Заголовки ответа на GET запрос фильтров для формы по id ссответствуют ожидаемым")
+    public void getFormFiltersValidateHeaders() {
+        // TODO
+    }
 
-    @Test
-    public void postSaveFormValidateHeaders() {}
+    @Test(groups = "HEADERS")
+    @Feature("Проверить заголовки")
+    @Story("TA-1В-5")
+    @Description("Заголовки ответа на POST запрос с JSON-обьектом ссответствуют ожидаемым")
+    public void postSaveFormValidateHeaders() {
+        // TODO
+    }
 }
