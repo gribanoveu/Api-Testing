bb7ed478b1ad83761938fd3c96702c9085f5a698
Evgeny Gribanov <gribanoveu@yandex.ru>
2021-12-23 15:01:51 +0300

    edit

--- src/test/java/tests/negative/InvalidData/StatusCode.java
+++ src/test/java/tests/negative/InvalidData/StatusCode.java
@@ -1,10 +1,57 @@
 package tests.negative.InvalidData;
 
+import io.qameta.allure.*;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 import util.LogListener;
 
 @Slf4j
 @Listeners(LogListener.class)
 public class StatusCode {
+
+    @Test(groups = { "SMOKE", "dbconnection" })
+    @Feature("Проверить код состояния")
+    @Story("TA-3А-1")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("")
+    public void dbConnectionInvalidLoginDataStatusCodeTest() {
+        // TODO
+    }
+
+    @Test(groups = { "SMOKE", "forms" })
+    @Feature("Проверить код состояния")
+    @Story("TA-3А-2")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("")
+    public void getFormsInvalidSessionIdStatusCodeTest() {
+        // TODO
+    }
+
+    @Test(groups = { "SMOKE", "form" })
+    @Feature("Проверить код состояния")
+    @Story("TA-3А-3")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("")
+    public void getFormByInvalidIdStatusCodeTest() {
+        // TODO
+    }
+
+    @Test(groups = { "SMOKE", "formfilters" })
+    @Feature("Проверить код состояния")
+    @Story("TA-3А-4")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("")
+    public void getFormFiltersByInvalidIdStatusCodeTest() {
+        // TODO
+    }
+
+    @Test(groups = { "SMOKE", "saveForm" })
+    @Feature("Проверить код состояния")
+    @Story("TA-3А-5")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("")
+    public void postSaveFormInvalidJsonStatusCodeTest() {
+        // TODO
+    }
 }
